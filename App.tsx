d66cdd46714dd015831351b18f9eb2625d0281a5
stogashi146 <stogashi146@gmail.com>
2023-03-28 09:30:05 +0900

    install_react-native-dropdown-picker

--- App.tsx
+++ App.tsx
@@ -4,8 +4,16 @@ import { DiaryBalanceList } from "./src/components/DiaryBalanceList";
 import { MonthSelect } from "./src/components/MonthSelect";
 import { IncomeExpenseTotal } from "./src/components/IncomeExpenseTotal";
 import { BalanceTotal } from "./src/components/BalanceTotal";
+import { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export default function App() {
+  const [open, setOpen] = useState(true);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    { label: "Apple", value: "apple" },
+    { label: "Banana", value: "banana" },
+  ]);
   return (
     <View style={styles.container}>
       <AppBar />
@@ -13,7 +21,21 @@ export default function App() {
       <IncomeExpenseTotal />
       <BalanceTotal />
       <View style={styles.sortContainer}>
-        <View></View>
+        <Text>aaa</Text>
+      </View>
+      <View>
+        <Text>aaa</Text>
+        <DropDownPicker
+          multiple={true}
+          min={0}
+          max={2}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
       </View>
       <DiaryBalanceList />
     </View>
